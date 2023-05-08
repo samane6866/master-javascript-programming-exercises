4c6f83623db2d0f3c87b43fecf2468b6c5ef66cf
samane6866 B <samane.bahramian@gmail.com>
2023-05-08 16:25:28 +0000

    some exercise done

--- exercises/060-removeFromFrontOfNew/app.js
+++ exercises/060-removeFromFrontOfNew/app.js
@@ -1,4 +1,5 @@
 function removeFromFrontOfNew(arr) {
     // your code here
-    
+    let newArr=arr.filter((array, index)=> index!=0)
+    return newArr
 }
--- exercises/061-countCharacter/app.js
+++ exercises/061-countCharacter/app.js
@@ -1,4 +1,4 @@
 function countCharacter(str, char) {
     // your code here
-    
+    return str.split("").filter((e)=> e===char).length
 }
